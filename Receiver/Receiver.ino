c60dda220cba4c8829a0838f1e63c53d7fb95fad
TRENT HAMILTON <trenthamilton@TRENTs-MacBook-Air.local>
2018-08-20 12:25:31 -0700

    new library

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -18,7 +18,7 @@ const byte address[6] = "00001";
 void setup() {
   Serial.begin(9600);
   radio.begin();
-  radio.openReadingPipe(0, address);
+  radio.openReadingPipe(1, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
